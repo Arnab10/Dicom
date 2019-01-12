f4adf2aed5a4ab4799a8b71d8eeebe2cb423e2c0
Arnab Bhattacharya <arnabhattacharya10@gmail.com>
2019-01-13 03:43:47 +0530

    Update dicom.py

--- dicom.py
+++ dicom.py
@@ -1,8 +1,8 @@
 import pydicom as pd
 
-filename1 = open("ttfm.dcm",'rb')                      #reading the file in read-binary mode
+filename1 = open("/home/arnab/Downloads/ttfm.dcm",'rb')                      #reading the file in read-binary mode
 ds1 = pd.filereader.dcmread(filename1,force=True)      #force Set to True to force reading even if no File Meta Information header is found
-filename2 = open("bmode.dcm",'rb')
+filename2 = open("/home/arnab/Downloads/bmode.dcm",'rb')
 ds2 = pd.filereader.dcmread(filename2,force=True)
 
 ds1.dir()                                             #display DICOM Tags of ttfm.dcm file
@@ -11,10 +11,10 @@ ds2.dir()                                             #display DICOM Tags of bmo
 
 
 #Writing the output in a .txt file
-file = open("output.txt",'w')
-file.write("The DICOM Tags in ttfm.dcm file : \n ")
+file = open("/home/arnab/Downloads/output.txt",'w')
+file.write("The DICOM Tags in ttfm.dcm file : \n\n ")
 file.write("{}".format(ds1.dir()))
-file.write("\nThe DICOM Tags of bmode.dcm file : \n")
+file.write("\n\n\n\nThe DICOM Tags of bmode.dcm file : \n\n")
 file.write("{}".format(ds2.dir()))
 
 #closing the files
