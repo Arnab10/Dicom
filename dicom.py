f31ae7cba9075197d6130ef51a5b6cf1f451abb9
Arnab Bhattacharya <arnabhattacharya10@gmail.com>
2019-01-13 16:01:03 +0530

    Update dicom.py

--- dicom.py
+++ dicom.py
@@ -1,23 +1,33 @@
 import pydicom as pd
 
+
 filename1 = open("/home/arnab/Downloads/ttfm.dcm",'rb')                      #reading the file in read-binary mode
-ds1 = pd.filereader.dcmread(filename1,force=True)      #force Set to True to force reading even if no File Meta Information header is found
+ds1 = pd.filereader.dcmread(filename1,force=True)                            #force Set to True to force reading even if no File Meta Information header is found
 filename2 = open("/home/arnab/Downloads/bmode.dcm",'rb')
 ds2 = pd.filereader.dcmread(filename2,force=True)
 
-ds1.dir()                                             #display DICOM Tags of ttfm.dcm file
-print("\n")
-ds2.dir()                                             #display DICOM Tags of bmode.dcm file
 
+file = open("/home/arnab/Downloads/output.txt",'w',encoding="utf-8")          #opening the output file .txt
 
-#Writing the output in a .txt file
-file = open("/home/arnab/Downloads/output.txt",'w')
 file.write("The DICOM Tags in ttfm.dcm file : \n\n ")
-file.write("{}".format(ds1.dir()))
-file.write("\n\n\n\nThe DICOM Tags of bmode.dcm file : \n\n")
-file.write("{}".format(ds2.dir()))
 
-#closing the files
+n = [[],[]]
+
+element1 = ds1.elements()
+element2 = ds2.elements()
+
+for elem,k in zip(element1,range(0,57)):
+    n = elem.tag
+    file.write(str(k+1)+". "+str(n)+" "+ds1.dir()[k]+"\n")
+    
+file.write("\n\n\nThe DICOM Tags in bmode.dcm file : \n\n")
+
+for elem,k in zip(element2,range(0,57)):
+    n=elem.tag
+    file.write(str(k+1)+". "+str(n)+" "+ds2.dir()[k]+"\n")
+    
+file.close()                                                                   #closing all files
 filename1.close()
 filename2.close()
-file.close()
+    
+    
